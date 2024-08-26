41b2483a2bcbe5afe2c68b68116a8012397ee6a7
Possanii <gpossanii@outlook.com>
2024-08-26 09:16:36 -0300

    feat: react to messages

--- web/src/components/message.tsx
+++ web/src/components/message.tsx
@@ -1,21 +1,56 @@
 import { ArrowUp } from "lucide-react";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
+import { toast } from "sonner";
+import { createMessageReaction } from "../http/create-message-reaction";
+import { removeMessageReaction } from "../http/remove-message-reaction";
 
-interface IMessageProps {
+interface MessageProps {
+  id: string;
   text: string;
   amountOfReactions: number;
   answered?: boolean;
 }
 
 export function Message({
+  id: messageId,
   text,
   amountOfReactions,
   answered = false,
-}: IMessageProps) {
+}: MessageProps) {
+  const { roomId } = useParams();
   const [hasReacted, setHasReacted] = useState(false);
 
-  function handleReactToMessage() {
-    setHasReacted((state) => !state);
+  if (!roomId) {
+    throw new Error("Messages components must be used within room page");
+  }
+
+  async function createMessageReactionAction() {
+    if (!roomId) {
+      return;
+    }
+
+    try {
+      await createMessageReaction({ messageId, roomId });
+    } catch {
+      toast.error("Falha ao reagir mensagem, tente novamente!");
+    }
+
+    setHasReacted(true);
+  }
+
+  async function removeMessageReactionAction() {
+    if (!roomId) {
+      return;
+    }
+
+    try {
+      await removeMessageReaction({ messageId, roomId });
+    } catch {
+      toast.error("Falha ao remover reação, tente novamente!");
+    }
+
+    setHasReacted(false);
   }
 
   return (
@@ -24,11 +59,12 @@ export function Message({
       className="ml-4 leading-relaxed text-zinc-100 data-[answered=true]:opacity-50 data-[answered=true]:pointer-events-none"
     >
       {text}
+
       {hasReacted ? (
         <button
           type="button"
-          onClick={handleReactToMessage}
-          className="flex items-center justify-center gap-2 mt-3 text-orange-400 text-sm font-medium hover:text-orange-500"
+          onClick={removeMessageReactionAction}
+          className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
         >
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
@@ -36,8 +72,8 @@ export function Message({
       ) : (
         <button
           type="button"
-          onClick={handleReactToMessage}
-          className="flex items-center justify-center gap-2 mt-3 text-zinc-400 text-sm font-medium hover:text-zinc-300"
+          onClick={createMessageReactionAction}
+          className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
         >
           <ArrowUp className="size-4" />
           Curtir pergunta ({amountOfReactions})
